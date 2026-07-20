99b9cbf4dd4341e7255c907f38d217189a312abf
YannKecot <ekaprets887@gmail.com>
2026-07-20 20:01:26 +0800

    Pendatang

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -95,7 +95,7 @@
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item"
-                                href="<?= base_url('user'); ?>">
+                                href="<?= base_url('user/user'); ?>">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     My Profile
                                 </a>
